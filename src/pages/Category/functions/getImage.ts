ff7e7414c60d40e44e7e3aeccacdf9325f262b73
Kevin Gervais <kevingervais@Kevins-MacBook-Pro.local>
2020-11-10 11:42:00 -0500

    update image

--- src/pages/Category/functions/getImage.ts
+++ src/pages/Category/functions/getImage.ts
@@ -1,16 +1,18 @@
-import { DuckDuckGoImage, image_search } from "duckduckgo-images-api"
+import GoogleImages from 'google-images'
 
 import { CategoryClass } from "../components/Category"
 
 export async function getImage(this: CategoryClass): Promise<string> {
-  const { titleList, languageList } = this.state
+  const { languageList } = this.state
   const enIndex = languageList.indexOf("en")
   const frIndex = languageList.indexOf("fr")
   const arIndex = languageList.indexOf("ar")
   const index = enIndex === -1 ? frIndex === -1 ? arIndex : frIndex : enIndex
-  const result: DuckDuckGoImage[] = await image_search({ query: titleList[index] })
+  const client = new GoogleImages('877397795c5259820', 'AIzaSyD5dxjKQnFJ_dHZ7p4tS5OkfuGst_-frfo')
+  const result: GoogleImages.Image[] = await client.search("Steve Angello", { safe: "off" })
+  console.log(result)
   if (result[0]) {
-    return result[0].thumbnail
+    return result[0].thumbnail.url
   } else {
     return ""
   }
