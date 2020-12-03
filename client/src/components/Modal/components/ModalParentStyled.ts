5d9b3985481c08e344f7345362424efb625ce911
Kevin Gervais <kevingervais@Kevins-MacBook-Pro.local>
2020-12-02 20:27:51 -0500

    small modal fix

--- client/src/components/Modal/components/ModalParentStyled.ts
+++ client/src/components/Modal/components/ModalParentStyled.ts
@@ -7,7 +7,7 @@ export const ModalParentStyled = styled.div<{}>`
   max-width: 600px;
   max-height: calc(100% - 100px);
   background-color: white;
-  width: calc(100% - 100px);
+  width: calc(100% - 20px);
   border-radius: 10px;
   box-shadow: 0 0 5px hsla(0, 0%, 0%, 0.3);
   &.full-screen {
@@ -21,12 +21,5 @@ export const ModalParentStyled = styled.div<{}>`
       width: calc(100% - 45px);
       text-align: center;
     }
-  }
-  @media (max-width: 500px) {
-    border-radius: 0 !important;
-    box-shadow: none !important;
-    width: 100% !important;
-    max-width: none;
-    max-height: 100% !important;
   }
 `
